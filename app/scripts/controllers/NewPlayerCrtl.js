b0d91893dce2aa26fb362c36331ec5be014de022
David Jensen <djensen@doner.com>
2015-01-30 10:34:53 -0500

    added dragging functionality to app

--- app/scripts/controllers/NewPlayerCrtl.js
+++ app/scripts/controllers/NewPlayerCrtl.js
@@ -11,23 +11,46 @@ angular.module('wordCannonApp')
   .controller('NewPlayerCrtl', function ($scope, $q, playerFactory, letterFactory) {
 
   	$scope.player = null;
-  	// function generateTiles(n){
-  	// 	letterFactory.getLetterRand(n)
- 		// .then(function(data)
- 		// {
- 		// 	//data returned from getLetterRand is letters array from letterHelper
- 		// 	letters = data;
- 		// 	console.log(letters);
- 		// });
-  	// }
+      $scope.centerAnchor = true;
+      $scope.draggableObjects = [];
+      $scope.droppedObjects = [];
+      $scope.discardCounter = 0;
+
+
+      $scope.toggleCenterAnchor = function(){
+        $scope.centerAnchor = !$scope.centerAnchor;
+      };
+
+
+
+      $scope.onDragSuccess = function(data,evt){
+        console.log('133', '$scope', 'onDragSuccess', '', evt);
+        var index = $scope.droppedObjects.indexOf(data);
+        if(index == -1){
+          $scope.droppedObjects.push(data);
+          console.log('drag index: ', index)
+        }
+      }
+      $scope.onDropSuccess = function(data,evt){
+        var index = $scope.droppedObjects.indexOf(data);
+        if(index == -1){
+          $scope.droppedObjects.push(data);
+        }
+        $scope.discardCounter++;
+        console.log('drop index: ', index);
+      }
+
+      var inArray = function(array, obj){
+        var index = array.indexOf(obj);
+      }
 
 	$scope.setPlayer = function(){
    		$scope.player = playerFactory.addPlayer($scope.tempName, $scope.tempHealth);
    		letterFactory.getLetterRand(8).then(function(data,error)
    		{
-   			$scope.player = playerFactory.setTiles(data);
-   			console.log($scope.player);
-   			console.log($scope.player.tiles.letter)
+   		     $scope.player = playerFactory.setTiles(data);
+                $scope.draggableObjects = $scope.player.tiles;
+                console.log($scope.draggableObjects);
    		})
 	}
   });
